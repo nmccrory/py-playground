9ef5562f6fe9ad16edad5da3b92354ffc05bda31
Nick McCrory <n.mccrory@icloud.com>
2015-07-13 15:08:32 -0700

    Starting on some basic python operations

---
+++ Basics/mult_five.py
@@ -0,0 +1,6 @@
+#) multiples of five up to one million
+#start with 100 so we don't crash it
+for count in range(0,100):
+	if(count%5 == 0):
+		print count
+	
---
+++ Basics/multiples.py
@@ -0,0 +1,4 @@
+#) odd from 1-50
+for count in range(1,50, 2):
+	print count
+	
