feee848c4bb41eb93c9de9d2e6878f45e4b67c42
Nick McCrory <n.mccrory@icloud.com>
2015-07-15 02:15:34 -0700

    Bike program completed

--- OOP/bikes.py
+++ OOP/bikes.py
@@ -1,4 +1,27 @@
 #) creating a bikes class
 
-class Bike:
-	def __init__(self, max_speed, price, miles)
+class Bike():
+	def __init__(self, max_speed, price, miles):
+		self.max_speed = max_speed
+		self.price = price
+		self.miles = miles
+
+	def displayinfo(self):
+		print "Price:",self.price
+		print "Speed:",self.max_speed
+		print "Miles:", self.miles
+		return self
+
+	def drive(self):
+		self.miles += 10
+		print 'Driving...'
+		return self
+
+	def reverse(self):
+		self.miles -= 5
+		print 'Reversing...'
+		return self
+
+bike1 = Bike(140, 20000, 60000)
+
+bike1.drive().drive().reverse().displayinfo()
